8ceadca8f022cc365e2eaf0c5467e5f49afc4fcd
red-jen <redaje102105@gmail.com>
2025-05-05 15:33:46 +0100

    Mur-124 fix the serve bug

--- MurArt/bootstrap/middleware.php
+++ MurArt/bootstrap/middleware.php
@@ -1,33 +1,52 @@
 <?php
 
-// return [
-//     // Web middleware...
-//     'web' => [
-//         \App\Http\Middleware\EncryptCookies::class,
-//         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-//         \Illuminate\Session\Middleware\StartSession::class,
-//         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-//         \App\Http\Middleware\VerifyCsrfToken::class,
-//         \Illuminate\Routing\Middleware\SubstituteBindings::class,
-//     ],
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-//     // API middleware...
-//     'api' => [
-//         \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
-//         \Illuminate\Routing\Middleware\SubstituteBindings::class,
-//     ],
-
-//     // Named middleware...
-//     'role' => \App\Http\Middleware\CheckRole::class,
-//     'auth' => \App\Http\Middleware\Authenticate::class,
-//     'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-//     'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
-//     'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-//     'can' => \Illuminate\Auth\Middleware\Authorize::class,
-//     'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-//     'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-//     'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
-//     'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-//     'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-//     'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-// ];
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        // Global middleware
+        $middleware->append([
+            \App\Http\Middleware\TrustProxies::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
+            \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+            \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+            \App\Http\Middleware\TrimStrings::class,
+            \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        ]);
+        
+        // Web group middleware
+        $middleware->web([
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
+        
+        // Register your named middleware here
+        $middleware->alias([
+            
+            'auth' => \App\Http\Middleware\Authenticate::class,
+            'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+            'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+            'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+            'can' => \Illuminate\Auth\Middleware\Authorize::class,
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+            'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+            'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+            'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+            'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+             'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })
+    ->create();
